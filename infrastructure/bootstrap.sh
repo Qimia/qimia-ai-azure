ac186a4cb49ddf26166dceb8cc9f2c1f6aebbcdd
Oguz Uzman <o.uzman@qimia.de>
2023-11-14 23:13:11 +0100

    fix: make the scripts importable as modules

--- infrastructure/bootstrap.sh
+++ infrastructure/bootstrap.sh
@@ -18,5 +18,5 @@ docker container stop $(docker container ls -aq) || true
 docker container rm $(docker container ls -aq) || true
 az acr login -n qimiaai27da  || true # It's okay to fail silently here. We'd find out in the next step anyway
 docker-compose pull
-docker-compose up --detach
+docker-compose up
 docker image prune -f
