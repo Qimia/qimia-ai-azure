b64b01f5495b1e5966e7ea1525ba52d5a7a68b5f
Oguz Uzman <o.uzman@qimia.de>
2024-01-19 23:06:19 +0100

    GPU implementation

--- infrastructure/bootstrap.sh
+++ infrastructure/bootstrap.sh
@@ -1,6 +1,18 @@
 set -e
 echo "Bootstrap path: $(pwd)"
-apt update && apt install -y docker.io docker-compose postgresql-client-common postgresql-client-12 wget curl
+echo "User: $USER"
+
+sudo apt update && sudo apt install -y docker.io postgresql-client-common postgresql-client-12 wget curl
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-docker2
+sudo systemctl restart docker
+sudo apt-get install ubuntu-drivers-common && sudo ubuntu-drivers autoinstall
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux
 tar -xvf azcopy.tar.gz
 rm azcopy.tar.gz
@@ -13,12 +25,11 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 usermod -aG docker ai_admin
 az login --identity
 echo "logged in."
-docker-compose down || true
+/usr/local/bin/docker-compose down || true
 docker container stop $(docker container ls -aq) || true
 docker container rm $(docker container ls -aq) || true
-az acr login -n qimiaai27da  || true # It's okay to fail silently here. We'd find out in the next step anyway
 docker image rm $(docker image ls -q) || true
-docker-compose pull
-docker-compose up --detach
+/usr/local/bin/docker-compose pull
+/usr/local/bin/docker-compose up --detach
 sleep 10
 docker image prune -f
